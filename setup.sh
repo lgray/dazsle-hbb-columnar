88a062796bffe03660583345b01130e112742c42
Lindsey Gray <Lindsey.Gray@cern.ch>
2019-02-06 12:16:41 -0600

    add awkward-numba acceleration

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 #this assumes you have jupyter notebook setup in some reasonable way already...
+pip install awkward-numba
+
 git clone https://github.com/lgray/dazsle-hbb-recipes.git .dazsle-hbb-recipes
 pushd .dazsle-hbb-recipes
 python setup.py install
